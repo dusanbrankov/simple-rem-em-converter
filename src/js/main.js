6c68bde6826873bacf61514fbfc242b10e1998f5
Dusan Brankov <dusanbrankov@pm.me>
2023-06-23 18:41:27 +0200

    Improve accessibility with ARIA attributes

--- src/js/main.js
+++ src/js/main.js
@@ -1,101 +1,93 @@
-// Variables
+const tabList = document.querySelectorAll('[role="tablist"]');
+const tabs = document.querySelectorAll('[role="tab"]');
+const tabPanel = document.querySelector('[role="tabpanel"]');
 
-const pxToRemTab = document.querySelector('#px-to-rem-tab');
-const pxToEmTab = document.querySelector('#px-to-em-tab');
+const basePx = document.querySelector("#base-px-input");
+const baseRemContainer = document.querySelector("#base-rem");
+const baseRem = document.querySelector("#base-rem-input");
+const removePrefix = document.querySelector("#rm-prefix");
+const px = document.querySelector("#px-input");
 
-const form = document.querySelector('#calc-form');
-const basePx = document.querySelector('#base-px');
-const baseRemContainer = document.querySelector('#base-rem-container');
-const baseRem = document.querySelector('#base-rem');
-const removePrefix = document.querySelector('#rm-prefix');
-const px = document.querySelector('#px');
+const result = document.querySelector("#output");
 
-const result = document.querySelector('#calc-result');
-
-const copyButton = document.querySelector('#copy');
-const copyMessage = document.querySelector('.copy-msg');
-
-// Convert
-
-form.addEventListener('submit', event => {
-  event.preventDefault();
-
-  if (!px.value || px.value === '0') {
-    return;
-  }
-
-  if (baseRemContainer.classList.contains('show')) {
-    const resultEm = `${+parseFloat(px.value / (baseRem.value * basePx.value)).toFixed(4)}em`;
-    calcResult(resultEm);
-  } else {
-    const resultRem = `${+parseFloat(px.value / basePx.value).toFixed(4)}rem`;
-    calcResult(resultRem);
-  }
-
-  copyButton.classList.add('show');
-});
+const copyButton = document.querySelector("#cp-button");
+const copyMessage = document.querySelector("#cp-message");
 
 // Tabs
-
-pxToEmTab.addEventListener('click', () => {
-  if (!pxToEmTab.classList.contains('active')) {
-    clear();
-  }
-  pxToRemTab.classList.remove('active');
-  pxToEmTab.classList.add('active');
-  baseRemContainer.classList.add('show');
+tabs.forEach(tab => {
+    tab.addEventListener("click", event => {
+        const activeTab = document.querySelector('[aria-selected="true"]');
+        if (activeTab !== event.currentTarget) {
+            clear();
+            activeTab.setAttribute("aria-selected", "false");
+            event.currentTarget.setAttribute("aria-selected", "true");
+            baseRemContainer.classList.toggle("show");
+        }
+    });
 });
 
-pxToRemTab.addEventListener('click', () => {
-  if (!pxToRemTab.classList.contains('active')) {
-    clear();
-  }
-  pxToEmTab.classList.remove('active');
-  pxToRemTab.classList.add('active');
-  baseRemContainer.classList.remove('show');
+// Convert
+tabPanel.addEventListener("submit", event => {
+    event.preventDefault();
+
+    if (!px.value || px.value === "0") {
+        return;
+    }
+
+    if (baseRemContainer.classList.contains("show")) {
+        const resultEm = `${+parseFloat(px.value / (baseRem.value * basePx.value)).toFixed(4)}em`;
+        calcResult(resultEm);
+    } else {
+        const resultRem = `${+parseFloat(px.value / basePx.value).toFixed(4)}rem`;
+        calcResult(resultRem);
+    }
+
+    copyButton.classList.add("show");
 });
 
 // Copy to clipboard
-
-copyButton.addEventListener('click', async () => {
-  try {
-    await navigator.clipboard.writeText(result.textContent);
-    copyMessage.classList.add('show');
-    setTimeout(() => {
-      copyMessage.classList.remove('show');
-    }, 3000);
-  } catch (err) {
-    console.error(err.name, err.message);
-  }
+copyButton.addEventListener("click", async () => {
+    try {
+        await navigator.clipboard.writeText(result.textContent);
+        copyMessage.textContent = "Copied to clipboard";
+        copyMessage.classList.add("show");
+        setTimeout(() => {
+            copyMessage.classList.remove("show");
+        }, 3000);
+    } catch (err) {
+        console.error(err.name, err.message);
+        copyMessage.textContent = "Something went wrong, please try again.";
+        copyMessage.classList.add("show");
+        setTimeout(() => {
+            copyMessage.classList.remove("show");
+        }, 3000);
+    }
 });
 
-// Function: Check if 'remove prefix' is checked
-
+// Check if "remove prefix" is checked
 const calcResult = (option) => {
-  const regex = /^0/;
+    const regex = /^0/;
 
-  if (removePrefix.checked) {
-    if (regex.test(option)) {
-      result.textContent = option.substring(1);
+    if (removePrefix.checked) {
+        if (regex.test(option)) {
+            result.textContent = option.substring(1);
+        } else {
+            result.textContent = option;
+        }
     } else {
-      result.textContent = option;
+        result.textContent = option;
     }
-  } else {
-    result.textContent = option;
-  }
 };
 
-// Function: Clear all fields
-
+// Clear all fields
 const clear = () => {
-  basePx.value = 16;
-  baseRem.value = 1;
-  removePrefix.checked = false;
-  px.value = '';
-  result.textContent = '';
-  copyButton.classList.remove('show');
+    basePx.value = 16;
+    baseRem.value = 1;
+    removePrefix.checked = false;
+    px.value = "";
+    result.textContent = "";
+    copyButton.classList.remove("show");
 };
 
-// Show current year in page footer
-
-document.querySelector('#year').textContent = new Date().getFullYear();
+// Show current year in site footer
+document.querySelector("#year").textContent = new Date().getFullYear();
